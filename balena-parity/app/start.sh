438b54604d924a50db841963d5eee6f76d5db80b
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-04-27 18:31:00 -0700

    parity: make sda available, dont run if mount fails

--- balena-parity/app/start.sh
+++ balena-parity/app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p /mnt/storage && mount /mnt/storage
-/usr/local/parity --base-path /mnt/storage --unsafe-expose --no-download
+mkdir -p /mnt/storage && mount /mnt/storage && \
+	/usr/local/parity --base-path /mnt/storage --unsafe-expose --no-download
