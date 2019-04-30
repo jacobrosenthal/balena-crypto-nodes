2103b3c6837e80c0b64b24b3cea0c9f265eaefa6
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-04-30 14:45:23 -0700

    parity: flags to fix out of memory crashes during sync

--- balena-parity/app/start.sh
+++ balena-parity/app/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p /mnt/storage && mount /mnt/storage && \
-	/usr/local/parity --base-path /mnt/storage --unsafe-expose --no-download
+	/usr/local/parity --base-path /mnt/storage \
+		--unsafe-expose --no-download --pruning "fast" --pruning-memory 16  --pruning-history 16 --cache-size 16
