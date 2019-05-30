d09d157175dc8c4fd105c58ad422949cecf210b2
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-05-29 19:41:54 -0700

    hns: enable wallet api

--- balena-hns/app/start.sh
+++ balena-hns/app/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+ARGS=""
+
+[ "$BALENAENV_HSD_COINBASE" ] && ARGS+=" --coinbase '$BALENAENV_HSD_COINBASE'"
+
 mkdir -p /mnt/storage && mount /mnt/storage && \
-	/usr/src/app/hsd/bin/hsd --prefix /mnt/storage/hns --http-host 0.0.0.0 --no-auth --log-level info
+	/usr/src/app/hsd/bin/hsd --prefix /mnt/storage/hns --log-level info $ARGS \
+		--http-host 0.0.0.0 --no-auth \
+		--wallet-http-host=0.0.0.0 --wallet-no-auth \
