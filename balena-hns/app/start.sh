3bfa8bb5565ae0f76bbbf6f42d3f80d713a6f2a0
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-05-29 18:01:10 -0700

    hsn: fix for Unauthorized (bad API key).

--- balena-hns/app/start.sh
+++ balena-hns/app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /mnt/storage && mount /mnt/storage && \
-	/usr/src/app/hsd/bin/hsd --prefix /mnt/storage/hns --http-host 0.0.0.0
+	/usr/src/app/hsd/bin/hsd --prefix /mnt/storage/hns --http-host 0.0.0.0 --no-auth
