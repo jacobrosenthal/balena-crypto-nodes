35b866adbf806b336d5ff6e234d97f8e767116a1
Jacob <jacobrosenthal@gmail.com>
2019-12-18 15:49:45 -0700

    geth: enable ws

--- balena-geth/app/start.sh
+++ balena-geth/app/start.sh
@@ -5,4 +5,4 @@ ARGS=""
 [ "$BALENAENV_CORS" ] && ARGS+=" --rpccorsdomain $BALENAENV_CORS"
 
 mkdir -p /mnt/storage/geth && mount /mnt/storage && \
- /usr/local/geth --rpc --rpcaddr "0.0.0.0" --rpcvhosts "*" --datadir /mnt/storage/geth $ARGS
+ /usr/local/geth --ws --wsaddr "0.0.0.0" --wsorigins "*" --rpc --rpcaddr "0.0.0.0" --rpcvhosts "*" --datadir /mnt/storage/geth $ARGS
