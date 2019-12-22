82a0cf37741f1310239734e412fb2362f14846c2
Jacob <jacobrosenthal@gmail.com>
2019-12-22 16:21:13 -0700

    0x-mesh

---
+++ balena-0x_mesh/app/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export VERBOSITY=5
+export DATA_DIR=/mnt/storage/0x_mesh/
+export RPC_ADDR=0.0.0.0:60557
+export P2P_TCP_PORT=60558
+export P2P_WEBSOCKETS_PORT=60559
+export ETHEREUM_RPC_URL=http://geth:8545
+export ETHEREUM_CHAIN_ID=1
+
+mkdir -p /mnt/storage/0x_mesh && mount /mnt/storage && /usr/local/mesh
