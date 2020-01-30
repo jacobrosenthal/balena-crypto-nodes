12e6250310d4c7942f35d7969d280b2ebf3efe64
Jacob <jacobrosenthal@gmail.com>
2020-01-30 16:24:11 -0700

    0xmesh v9.0.0

--- balena-0x_mesh/app/start.sh
+++ balena-0x_mesh/app/start.sh
@@ -3,7 +3,7 @@
 if [ "$BALENAENV_MESH_LOGS" ]; then export VERBOSITY=$BALENAENV_MESH_LOGS; fi
 
 export DATA_DIR=/mnt/storage/0x_mesh/
-export RPC_ADDR=0.0.0.0:60557
+export WS_RPC_ADDR=0.0.0.0:60557
 export P2P_TCP_PORT=60558
 export P2P_WEBSOCKETS_PORT=60559
 export ETHEREUM_RPC_URL=http://geth:8545
