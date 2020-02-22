2c2217a579fb98951d1069a5aa1e32bf2ecc4407
Jacob <jacobrosenthal@gmail.com>
2020-02-21 21:21:53 -0700

    turbogeth

--- balena-geth/app/start.sh
+++ balena-geth/app/start.sh
@@ -6,4 +6,4 @@ ARGS=""
 [ "$BALENAENV_GETH_VERBOSITY" ] && ARGS+=" --verbosity $BALENAENV_GETH_VERBOSITY"
 
 mkdir -p /mnt/storage/geth && mount /mnt/storage && \
- /usr/local/geth --syncmode light --cache 256 --ws --wsaddr "0.0.0.0" --wsorigins "*" --rpc --rpcaddr "0.0.0.0" --rpcvhosts "*" --datadir /mnt/storage/geth $ARGS
+ /usr/local/geth --cache 256 --ws --wsaddr "0.0.0.0" --wsorigins "*" --rpc --rpcaddr "0.0.0.0" --rpcvhosts "*" --datadir /mnt/storage/geth $ARGS
